fe8acddc0ff3f8d9d2549ec8dc5f0a834a68ce56
AKA-Truc <pthanhtruca3@gmail.com>
2024-06-21 23:17:33 +0700

    Delete admin directory

--- admin/order/order.js
+++
@@ -1,8 +0,0 @@
-const viewBtn = document.querySelectorAll('.view-btn');
-viewBtn.forEach(button => {
-    button.addEventListener('click',function(){
-        window.location.href ='../order/detail.html';
-    });
-});
-
-
