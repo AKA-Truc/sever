408ac8db0683799c7bb04b5fe74a15bd4c701579
AKA-Truc <pthanhtruca3@gmail.com>
2024-06-21 21:37:15 +0700

    Delete Routes directory

--- Routes/invoice_routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createInvoice, getAllInvoice, getInvoice, updateInvoice } = require('../controllers/invoice_controller');
-const router = express.Router();
-
-router.post('/createInvoice', createInvoice);//ok
-router.get('/getAllInvoice', getAllInvoice);//
-router.get('/getInvoice/:iid', getInvoice);//ok
-
-module.exports = router;
